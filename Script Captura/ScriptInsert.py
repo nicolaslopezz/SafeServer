1ace8bbe52e5f105b50245dcf83972bda0dbf07f
nicolaslopezz <nicolas.pereira@sptech.school>
2024-09-16 13:26:33 -0300

    scriptPythonAtualizacoes

--- Script Captura/ScriptInsert.py
+++ Script Captura/ScriptInsert.py
@@ -4,9 +4,9 @@ import mysql.connector
 
 # Configurações do banco de dados
 db_config = {
-    'host': '',
+    'host': 'localhost',
     'user': 'root',
-    'password': '',
+    'password': 'palmeiras21',
     'database': 'SafeServer'
 }
 
@@ -20,30 +20,27 @@ def monitorar_e_enviar_dados(servidor_id):
     while True:
         memoria = psutil.virtual_memory()
         cpu = psutil.cpu_percent(interval=1)
-        disco = psutil.disk_usage('/')
+        
 
-        servidor_id = '(ALTERAR PARA QUAL O ID DO SERVIDOR DA SUA MAQUINA(1 OU 2)'
+        servidor_id = 2
 
         total_memoriagb = memoria.total / (1024 ** 3)
-        used_memoriagb = memoria.used / (1024 ** 3)
-        total_discogb = disco.total / (1024 ** 3)
-        used_discogb = disco.used / (1024 ** 3)
-        livre_memoriagb = total_memoriagb - used_memoriagb
-        livre_discogb = total_discogb - used_discogb
+        used_memoriagb = round(memoria.used / (1024 ** 3),2)
+        livre_memoriagb = round((total_memoriagb - used_memoriagb),2)
+        
 
         query = '''
-        INSERT INTO registro (percent_use_cpu, uso_ram_gb, livre_ram_gb, total_ram_gb, uso_disco_gb, livre_disco_gb, total_disco_gb, fkServidor)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO registro (percent_use_cpu, uso_ram_gb, livre_ram_gb, fkServidor)
+        VALUES (%s, %s, %s, %s)
         '''
-        values = (cpu, used_memoriagb, livre_memoriagb, total_memoriagb, used_discogb, livre_discogb, total_discogb, servidor_id)
+        values = (cpu, used_memoriagb, livre_memoriagb, servidor_id)
 
         meucursor.execute(query, values)
         meusql.commit()
 
         # Impressão dos dados inseridos
         print(f"Dados inseridos: CPU: {cpu:.1f}%, RAM Usada: {used_memoriagb:.1f} GB, RAM Livre: {livre_memoriagb:.1f} GB, "
-              f"Total RAM: {total_memoriagb:.1f} GB, Disco Usado: {used_discogb:.1f} GB, Disco Livre: {livre_discogb:.1f} GB, "
-              f"Total Disco: {total_discogb:.1f} GB")
+              f"Total RAM: {total_memoriagb:.1f} GB ")
 
         time.sleep(2)
 
