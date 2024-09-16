824535730e6f1452dbacd8587c0b61a9aaf04c5e
GiorgioLA <Giorgio.Antunes@sptech.school>
2024-09-16 12:50:58 -0300

    continuando a parametrizar o web-data-viz

--- web-data-viz - SafeServer/app.js
+++ web-data-viz - SafeServer/app.js
@@ -17,7 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +26,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
