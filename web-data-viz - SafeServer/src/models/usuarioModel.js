31a75d3d440c1b364f53bce6d53f8df46fe62b1f
GiorgioLA <Giorgio.Antunes@sptech.school>
2024-10-07 13:57:29 -0300

    Adicionando cargo ao cadastro

--- web-data-viz - SafeServer/src/models/usuarioModel.js
+++ web-data-viz - SafeServer/src/models/usuarioModel.js
@@ -10,17 +10,13 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-<<<<<<< HEAD
-function cadastrar(nome, email, cpf, senha, fkEmpresa) {
-=======
-function cadastrar(nome, email, senha, cpf, fkEmpresa) {
->>>>>>> b98183120c83a218a2438e0f6db57d4fe5637a57
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+function cadastrar(nome, email, senha, cpf, fkEmpresa, cargo) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa, cargo);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO funcionario (nome, email, cpf, senha, fkEmpresa) VALUES ('${nome}', '${email}', '${cpf}', '${senha}', '${fkEmpresa}');
+        INSERT INTO funcionario (nome, email, cpf, senha, cargo, fkEmpresa) VALUES ('${nome}', '${email}', '${cpf}', '${senha}', '${cargo}', '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
