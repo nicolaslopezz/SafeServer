ca6cf4927398e294f20c13babafef5ad7ed1ef81
LeandroAnjosHub <leandro.anjos@sptech.school>
2024-10-07 12:54:25 -0300

    Mudança rota

--- web-data-viz - SafeServer/src/controllers/usuarioController.js
+++ web-data-viz - SafeServer/src/controllers/usuarioController.js
@@ -47,7 +47,6 @@ function cadastrar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var cpf = req.body.cpfServer;
-    var telefone = req.body.telefoneServer;
     var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
@@ -55,18 +54,16 @@ function cadastrar(req, res) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
-    } else if(telefone == undefined) {
-        res.status(400).send("Seu telefone está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
     } else if (cpf == undefined){
         res.status(400).send("Seu CPF está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
     } else if (fkEmpresa == undefined) {
         res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, telefone, senha, cpf, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, cpf, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
