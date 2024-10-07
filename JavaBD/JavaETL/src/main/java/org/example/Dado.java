c2a4422bb5b6d6a4a6e713068e5e29a3dc5368e5
LeandroAnjosHub <leandro.anjos@sptech.school>
2024-10-07 18:23:11 -0300

    Arquivo Json gerado em Python

---
+++ JavaBD/JavaETL/src/main/java/org/example/Dado.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Dados {
+
+    @JsonProperty("CPU%")
+    private Double dados_cpu;
+
+    @JsonProperty("RAMGB")
+    private Double dados_ram;
+
+    @JsonProperty("REDE_RECV")
+    private Double dados_rede;
+
+    public Double getDados_cpu() {
+        return dados_cpu;
+    }
+
+    public void setDados_cpu(Double dados_cpu) {
+        this.dados_cpu = dados_cpu;
+    }
+
+    public Double getDados_ram() {
+        return dados_ram;
+    }
+
+    public void setDados_ram(Double dados_ram) {
+        this.dados_ram = dados_ram;
+    }
+
+    public Double getDados_rede() {
+        return dados_rede;
+    }
+
+    public void setDados_rede(Double dados_rede) {
+        this.dados_rede = dados_rede;
+    }
+}
