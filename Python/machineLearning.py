075617cdf58827e0ed09e1b0ff8db476bb787e0a
JulianaFreires <juliana.silva@sptech.school>
2024-12-05 21:50:00 -0300

    att machine learning conectada no bd

--- Python/machineLearning.py
+++ Python/machineLearning.py
@@ -2,43 +2,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pymysql
 
 file_path = "C:\\Users\\JuuhF\\Videos\\dadosColetados.xlsx"
 
 try:
     df = pd.read_excel(file_path)
-    
-    # convertendo para tipo numérico
+
     df['percent_use_cpu'] = pd.to_numeric(df['percent_use_cpu'], errors='coerce')
     df['recebido_rede'] = pd.to_numeric(df['recebido_rede'], errors='coerce')
-    # remover NaN
-    df = df.dropna(subset=['percent_use_cpu', 'recebido_rede'])
     
-
+    # Remover NaN
+    df = df.dropna(subset=['percent_use_cpu', 'recebido_rede'])
 except FileNotFoundError:
     print(f"Erro: Arquivo '{file_path}' não encontrado.")
 except Exception as e:
     print(f"Ocorreu um erro ao importar o arquivo: {e}")
 
-x = df['percent_use_cpu'].values.reshape(-1, 1)  #  variável independente
-y = df['recebido_rede'].values  # variável dependente
 
-# divisão dos dados em treino e teste
+x = df['recebido_rede'].values.reshape(-1, 1)  
+y = df['percent_use_cpu'].values 
+
+
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=50)
 
-# treinamento o modelo de regressão linear
 modelo = LinearRegression()
 modelo.fit(X_train, y_train)
-
 
 y_pred = modelo.predict(X_test)
 r2_score = modelo.score(X_test, y_test)
 print(f"R² do modelo: {r2_score}")
 
-# Regressão linear
+
 plt.scatter(x, y, color='blue')
 plt.plot(x, modelo.predict(x), color='red')
-plt.title('Regressão Linear - CPU (%) vs REDE_RECEBIDA (GB)')
-plt.xlabel('CPU%')
-plt.ylabel('REDE_REC')
+plt.title('Regressão Linear - REDE_RECEBIDA (GB) vs CPU (%)')
+plt.xlabel('REDE_RECEBIDA (GB)')
+plt.ylabel('CPU%')
 plt.show()
+
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',        
+    user='root',               
+    password='batatas123',    
+    database='SafeServer'      
+)
+
+
+cursor = conn.cursor()
+
+for i in range(len(X_test)):
+    cursor.execute("""
+        INSERT INTO regressao_linear (percent_use_cpu, recebido_rede, predicted_value)
+        VALUES (%s, %s, %s)
+    """, (y_test[i], X_test[i][0], y_pred[i]))  
+
+
+conn.commit()
+
+print("Dados inseridos na tabela")
+
+
+cursor.close()
+conn.close()
